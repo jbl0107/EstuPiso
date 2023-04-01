eb833bc30fa150f18006eeccc9791c4b4c416561
jbl0107 <ballestero20017@gmail.com>
2023-04-01 17:01:05 +0200

    #15 Se ha cambiado el modelo de usuario

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,11 +1,50 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from phonenumber_field.modelfields import PhoneNumberField
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 
+class UserManager(BaseUserManager):
+
+    def create_user(self, dni, name, surname, username, email, telephone, photo, password=None):
+        if not email:
+            raise ValueError('The user must have an email')
+
+        user = self.model(
+            dni=dni,
+            name=name,
+            surname=surname,
+            username=username,
+            email=self.normalize_email(email),
+            telephone=telephone,
+            photo=photo
+
+        )
+
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, dni, name, surname, username, email, telephone, photo, password):
+        user = self.create_user(
+            dni=dni,
+            name=name,
+            surname=surname,
+            username=username,
+            email=self.normalize_email(email),
+            telephone=telephone,
+            photo=photo,
+            password=password
+        )
+
+        user.isAdministrator = True
+        user.save()
+        return user
+
+
 class User(AbstractBaseUser):
+    dni = models.CharField(max_length=9, null=False, blank=False, unique=True)
     name = models.CharField(max_length=30, blank=False, null=False)
     surname = models.CharField(max_length=50, blank=False, null=False)
     username = models.CharField(max_length=30, unique=True, blank=False, null=False)
@@ -14,21 +53,28 @@ class User(AbstractBaseUser):
     telephone = PhoneNumberField(null = False, blank = False)
     photo = models.ImageField(upload_to='users/', null=True, blank=True)
     isActive = models.BooleanField(default=True)
+    isAdministrator = models.BooleanField(default=False)
 
-
-
-class Admin(AbstractBaseUser):
-    dni = models.CharField(max_length=9, null=False, blank=False, unique=True)
-    name = models.CharField(max_length=30, blank=False, null=False)
-    surname = models.CharField(max_length=50, blank=False, null=False)
-    username = models.CharField(max_length=30, unique=True, blank=False, null=False)
-    password = models.CharField(max_length=30, blank=False, null=False)
-    
+    last_login = False
+    objetcs = UserManager()
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'name', 'surname', 'dni', 'telephone', 'photo']
 
     def __str__(self):
-        return self.name + self.surname
+        return f'{self.name}, {self.surname}'
     
+    def has_perm(self, perm, obj = None):
+        return True
     
+    def has_module_perms(self, app_label):
+        return True
+    
+    @property
+    def is_staff(self):
+        return self.isAdministrator
+
+
+
 
 class Student(User):
 
@@ -84,14 +130,13 @@ class Property(models.Model):
     
 
 #Related_name sirve para poner nombre a la relacion inversa. Por ejemplo, podria acceder a todos los mensajes enviados por
-# un admin concreto de la siguiente forma: admin.mensajesEnviadosA.all() 
+# un user concreto de la siguiente forma: admin.sentMessagesU.all() 
 class Message(models.Model):
     date = models.DateTimeField(auto_now_add=True, null=False, blank=False)
     content = models.TextField(null=False, blank=False)
     userSender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sentMessagesU',null=True, blank=True)
     userRecipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receivedMessagesU', null=True, blank=True)
-    adminSender = models.ForeignKey(Admin, on_delete=models.CASCADE, related_name='sentMessagesA', null=True, blank=True)
-    adminRecipient = models.ForeignKey(Admin, on_delete=models.CASCADE, related_name='receivedMessagesA',null=True, blank=True)
+
 
 
 
@@ -118,6 +163,7 @@ class PropertyValoration(models.Model):
 
 
 # on_delete = cascade --> si borro un student, se borran todas sus reservas grupales.
+# admin es un user que debe tener isAdministrator = True
 class GroupReservation(models.Model):
     size = models.IntegerField(blank=False, null=False)
     destination = models.CharField(max_length=50 ,blank=False, null=False)
@@ -125,7 +171,7 @@ class GroupReservation(models.Model):
     email = models.EmailField(blank=False, null=False)
     duration = models.CharField(max_length=30, blank=False, null=False)
     student = models.ForeignKey(Student, null=False, on_delete=models.CASCADE, related_name="groupReservationStudent")
-    admin = models.ForeignKey(Admin, null=False, on_delete=models.DO_NOTHING, related_name="groupReservationAssigned") 
+    admin = models.ForeignKey(User, null=False, on_delete=models.DO_NOTHING, related_name="groupReservationAssigned") 
 
 
 
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -129,3 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = "api.User"
+
+
