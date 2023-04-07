3652b59e589cd069a6301cce5a48f35acb6d7926
jbl0107 <ballestero20017@gmail.com>
2023-04-07 18:13:16 +0200

    #48 Se han rehecho los decoradores mencionados Tambien se indicado en settings.py que JWT tome el user personalizado

--- backend/apis/permissions_decorators.py
+++ backend/apis/permissions_decorators.py
@@ -1,16 +1,15 @@
-from rest_framework import permissions
 from .models import Student, Owner
+from rest_framework.permissions import BasePermission
 
-def is_student(user):
-    return user.is_authenticated and user.is_instance_of(Student)
 
-def is_owner(user):
-    return user.is_authenticated and user.is_instance_of(Owner)
 
-class AccesoEstudiante(permissions.BasePermission):
+class IsStudent(BasePermission):
     def has_permission(self, request, view):
-        return is_student(request.user)
+        return request.user.is_authenticated and Student.objects.filter(id=request.user.id).exists()
+    
 
-class AccesoPropietario(permissions.BasePermission):
+
+class IsOwner(BasePermission):
     def has_permission(self, request, view):
-        return is_owner(request.user)
+        return request.user.is_authenticated and Owner.objects.filter(id=request.user.id).exists()
+     
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -145,6 +145,7 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
+    'USER_MODEL': "apis.User",
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
