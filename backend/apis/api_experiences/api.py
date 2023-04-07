1ff278b07e3ef54d8b0aeb41a18c3a9deb6be09e
jbl0107 <ballestero20017@gmail.com>
2023-04-07 19:59:33 +0200

    #34 Autenticación para la API de Experience implementada

--- backend/apis/api_experiences/api.py
+++ backend/apis/api_experiences/api.py
@@ -1,13 +1,17 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework import status
-
+from rest_framework.permissions import IsAuthenticated
+from apis.permissions_decorators import IsStudentOrAdmin
 
 from apis.models import Experience, Student
 from .serializers import ExperienceSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 @api_view(['GET', 'POST'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated, IsStudentOrAdmin])
 def experience_api_view(request):
 
     if request.method == 'GET':
@@ -20,6 +24,9 @@ def experience_api_view(request):
     #create
     elif request.method == 'POST':
         data = request.data
+        if int(data.get('student')) != request.user.id and request.user.isAdministrator == False:
+            return Response({'message': 'No puedes crear una experiencia para otro estudiante'}, status=status.HTTP_400_BAD_REQUEST) 
+
         serializer = ExperienceSerializer(data = data) 
         if serializer.is_valid():
             serializer.save()
@@ -27,8 +34,11 @@ def experience_api_view(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-from django.db import models
+
+
 @api_view(['GET', 'PUT', 'DELETE'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated, IsStudentOrAdmin])
 def experience_detail_api_view(request, id):
         
     # queryset
@@ -41,39 +51,47 @@ def experience_detail_api_view(request, id):
             serializer = ExperienceSerializer(experience)
             return Response(serializer.data, status=status.HTTP_200_OK)
         
-        elif request.method == 'PUT':
-            data = request.data
-            data['student'] = data['student']
-            serializer = ExperienceSerializer(experience, data = data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-        elif request.method == 'DELETE':
-            experience.delete()
-            return Response({'message':"Experiencia eliminada correctamente!"}, status=status.HTTP_200_OK)
+        if request.user.id == experience.student.id or request.user.isAdministrator:
+            
+            if request.method == 'PUT':
+                data = request.data
+                serializer = ExperienceSerializer(experience, data = data)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(serializer.data, status=status.HTTP_200_OK)
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            
+            elif request.method == 'DELETE':
+                
+                experience.delete()
+                return Response({'message':"Experiencia eliminada correctamente!"}, status=status.HTTP_200_OK)
+                
+        else:
+            return Response({'message':"Esta experiencia no es suya"}, status=status.HTTP_403_FORBIDDEN)
         
     return Response({'message':"No se ha encontrado una experiencia con estos datos"}, status=status.HTTP_400_BAD_REQUEST)
 
 
 
 @api_view(['GET'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated, IsStudentOrAdmin])
 def experience_student_api_view(request, id):
 
    student = Student.objects.filter(id=id).first()
 
    if student:
     
-    if request.method == 'GET':
-        all_experiences = Experience.objects.all()
-        res = []
+    if request.user.id == id or request.user.isAdministrator:
 
-        for p in all_experiences:
-            if p.student == student:
-                res.append(p)
-        
-        serializer = ExperienceSerializer(res, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-        
+        if request.method == 'GET':
+            
+            experiences = Experience.objects.filter(student=student)
+            serializer = ExperienceSerializer(experiences, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+    else:
+        return Response({'message': 'Solo puede obtener todas sus experiencias'}, status=status.HTTP_400_BAD_REQUEST)
+
+   else:
+
     return Response({'message': 'No se ha encontrado un estudiante con estos datos'}, status=status.HTTP_400_BAD_REQUEST)
