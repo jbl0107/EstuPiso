754b48734a6f703cfebe933ef33adc6063eeb38e
jbl0107 <ballestero20017@gmail.com>
2023-04-07 19:58:52 +0200

    #48 Se han redefinido los decoradores mencionados

--- backend/apis/permissions_decorators.py
+++ backend/apis/permissions_decorators.py
@@ -3,13 +3,14 @@ from rest_framework.permissions import BasePermission
 
 
 
-class IsStudent(BasePermission):
+class IsStudentOrAdmin(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and Student.objects.filter(id=request.user.id).exists()
+        return (request.user.is_authenticated and Student.objects.filter(id=request.user.id).exists()) or (request.user.isAdministrator and request.user.is_authenticated) 
     
 
 
-class IsOwner(BasePermission):
+class IsOwnerOrAdmin(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and Owner.objects.filter(id=request.user.id).exists()
-     
+        return (request.user.is_authenticated and Owner.objects.filter(id=request.user.id).exists()) or (request.user.isAdministrator and request.user.is_authenticated) 
+    
+
