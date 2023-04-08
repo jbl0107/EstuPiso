aad29f61b2e2de65c62f9e93e3adfbcbed12a855
jbl0107 <ballestero20017@gmail.com>
2023-04-08 17:47:37 +0200

    #37 Seguridad añadida a la API de InterestServiceProperty

--- backend/apis/api_interestServicesProperty/api.py
+++ backend/apis/api_interestServicesProperty/api.py
@@ -1,35 +1,63 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework import status
 
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from apis.models import InterestServiceProperty, Property
 from .serializers import InterestServicePropertySerializer
 
 
-@api_view(['GET', 'POST'])
+@api_view(['GET'])
 def interestServiceProperty_api_view(request):
 
     if request.method == 'GET':
 
-        #queryset
         interestServicesProperty = InterestServiceProperty.objects.all()
         serializer = InterestServicePropertySerializer(interestServicesProperty, many=True)
         return Response(serializer.data, status = status.HTTP_200_OK)
     
-    #create
-    elif request.method == 'POST':
+
+
+
+
+@api_view(['POST'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
+def interestServiceProperty_post_api_view(request):
+
+    if request.method == 'POST':
+
+        if not request.user.isAdministrator:
+            return Response({"message":"Debe ser administrador para crear un servicio de interés de un inmueble"}, status=status.HTTP_400_BAD_REQUEST)
+        
         data = request.data
         serializer = InterestServicePropertySerializer(data = data) 
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status = status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+@api_view(['GET'])
+def interestServiceProperty_get_detail_api_view(request, id):
+ 
+    interestServiceProperty = InterestServiceProperty.objects.filter(id=id).first()
+
+    if interestServiceProperty:
+        
+        if request.method == 'GET':
+            serializer = InterestServicePropertySerializer(interestServiceProperty)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+    return Response({'message':"No se ha encontrado un servicio de interés de una propiedad con estos datos"}, status=status.HTTP_400_BAD_REQUEST)
 
 
 
-
-@api_view(['GET', 'PUT', 'DELETE'])
+@api_view(['PUT', 'DELETE'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
 def interestServiceProperty_detail_api_view(request, id):
         
     # queryset
@@ -38,11 +66,11 @@ def interestServiceProperty_detail_api_view(request, id):
     # validacion
     if interestServiceProperty:
         
-        if request.method == 'GET':
-            serializer = InterestServicePropertySerializer(interestServiceProperty)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+        if not request.user.isAdministrator:
+            return Response({"message":"Debe ser administrador para poder realizar esta operación"}, status=status.HTTP_400_BAD_REQUEST)
         
-        elif request.method == 'PUT':
+        
+        if request.method == 'PUT':
             data = request.data
             serializer = InterestServicePropertySerializer(interestServiceProperty, data = data)
             if serializer.is_valid():
--- backend/apis/urls.py
+++ backend/apis/urls.py
@@ -11,7 +11,7 @@ from apis.api_valoration.api import valoration_user_api_view, valoration_user_de
 from apis.api_groupReservations.api import groupReservation_api_view, groupReservation_detail_api_view, groupReservation_student_api_view, groupReservation_post_api_view
 from apis.api_experiences.api import experience_api_view, experience_detail_api_view, experience_student_api_view
 from apis.api_interestServices.api import interestService_api_view, interestService_detail_api_view, interestService_post_api_view, interestService_get_detail_api_view
-from apis.api_interestServicesProperty.api import interestServiceProperty_api_view, interestServiceProperty_detail_api_view, interestServiceProperty_property_api_view
+from apis.api_interestServicesProperty.api import interestServiceProperty_api_view, interestServiceProperty_detail_api_view, interestServiceProperty_property_api_view, interestServiceProperty_post_api_view, interestServiceProperty_get_detail_api_view
 from apis.api_studentAnnouncements.api import studentAnnouncement_api_view, studentAnnouncement_detail_api_view, studentAnnouncement_student_api_view
 
 
@@ -78,10 +78,12 @@ urlpatterns = [
 
     path('interestServices/', interestService_api_view, name='interestService_api'),
     path('interestServices/new/', interestService_post_api_view, name='interestService_post_api'),
-    path('interestServices/details/<int:id>', interestService_get_detail_api_view, name='interestService_detail_api'),
+    path('interestServices/details/<int:id>', interestService_get_detail_api_view, name='interestService_get_detail_api'),
     path('interestServices/<int:id>', interestService_detail_api_view, name='interestService_detail_api'),
 
     path('interestServicesProperty/', interestServiceProperty_api_view, name='interestServiceProperty_api'),
+    path('interestServicesProperty/new/', interestServiceProperty_post_api_view, name='interestServiceProperty_post_api'),
+    path('interestServicesProperty/details/<int:id>', interestServiceProperty_get_detail_api_view, name='interestServiceProperty_get_detail_api'),
     path('interestServicesProperty/<int:id>', interestServiceProperty_detail_api_view, name='interestServiceProperty_detail_api'),
     path('interestServicesProperty/property/<int:id>', interestServiceProperty_property_api_view, name='interestServicesProperty_property_api'),
 
