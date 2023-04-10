c5f8918699c6b0bd8f737eff742b8567f7fa1faa
jbl0107 <ballestero20017@gmail.com>
2023-04-10 16:54:49 +0200

    #56 Campo "student" definido como "solo lectura"

--- backend/apis/api_experiences/api.py
+++ backend/apis/api_experiences/api.py
@@ -2,17 +2,35 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from apis.permissions_decorators import IsStudentOrAdmin
+from apis.permissions_decorators import IsStudentOrAdmin, IsStudent
 
 from apis.models import Experience, Student
 from .serializers import ExperienceSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.exceptions import PermissionDenied
 
 
 @api_view(['GET', 'POST'])
 @authentication_classes([JWTAuthentication])
-@permission_classes([IsAuthenticated, IsStudentOrAdmin])
 def experience_api_view(request):
+
+
+    def check_permissions(request):
+
+        if request.method == 'GET':
+            for permission_class in [IsAuthenticated, IsStudentOrAdmin]:
+                permission = permission_class()
+                if not permission.has_permission(request, None):
+                    raise PermissionDenied(getattr(permission, 'message', None))
+
+        if request.method == 'POST':
+            for permission_class in [IsAuthenticated, IsStudent]:
+                permission = permission_class()
+                if not permission.has_permission(request, None):
+                    raise PermissionDenied(getattr(permission, 'message', None))
+
+    check_permissions(request)
+
 
     if request.method == 'GET':
 
@@ -24,7 +42,8 @@ def experience_api_view(request):
     #create
     elif request.method == 'POST':
         data = request.data
-        if int(data.get('student')) != request.user.id and request.user.isAdministrator == False:
+        data['student'] = request.user.id
+        if int(data.get('student')) != request.user.id:
             return Response({'message': 'No puede crear una experiencia para otro estudiante'}, status=status.HTTP_403_FORBIDDEN) 
 
         serializer = ExperienceSerializer(data = data) 
@@ -38,8 +57,29 @@ def experience_api_view(request):
 
 @api_view(['GET', 'PUT', 'DELETE'])
 @authentication_classes([JWTAuthentication])
-@permission_classes([IsAuthenticated, IsStudentOrAdmin])
 def experience_detail_api_view(request, id):
+
+    def check_permissions(request):
+
+        if request.method == 'GET':
+            for permission_class in [IsAuthenticated, IsStudentOrAdmin]:
+                permission = permission_class()
+                if not permission.has_permission(request, None):
+                    raise PermissionDenied(getattr(permission, 'message', None))
+
+        if request.method == 'PUT':
+            for permission_class in [IsAuthenticated, IsStudent]:
+                permission = permission_class()
+                if not permission.has_permission(request, None):
+                    raise PermissionDenied(getattr(permission, 'message', None))
+                
+        elif request.method == 'DELETE':
+            for permission_class in [IsAuthenticated, IsStudentOrAdmin]:
+                permission = permission_class()
+                if not permission.has_permission(request, None):
+                    raise PermissionDenied(getattr(permission, 'message', None))
+
+    check_permissions(request)
         
     # queryset
     experience = Experience.objects.filter(id=id).first()
@@ -51,23 +91,29 @@ def experience_detail_api_view(request, id):
             serializer = ExperienceSerializer(experience)
             return Response(serializer.data, status=status.HTTP_200_OK)
         
-        if request.user.id == experience.student.id or request.user.isAdministrator:
+        
             
-            if request.method == 'PUT':
-                data = request.data
-                serializer = ExperienceSerializer(experience, data = data)
-                if serializer.is_valid():
-                    serializer.save()
-                    return Response(serializer.data, status=status.HTTP_200_OK)
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if request.method == 'PUT':
+
+            if request.user.id != experience.student.id:
+                 return Response({'message':"No puede actualizar una experiencia que no le pertenece"}, status=status.HTTP_403_FORBIDDEN)
+
+            data = request.data
+            data['student'] = request.user.id
+            serializer = ExperienceSerializer(experience, data = data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        elif request.method == 'DELETE':
+            if request.user.id != experience.student.id and not request.user.isAdministrator:
+                return Response({'message':"No puede eliminar una experiencia que no le pertenece"}, status=status.HTTP_403_FORBIDDEN)
+           
+            experience.delete()
+            return Response({'message':"Experiencia eliminada correctamente!"}, status=status.HTTP_200_OK)
+                
             
-            elif request.method == 'DELETE':
-                
-                experience.delete()
-                return Response({'message':"Experiencia eliminada correctamente!"}, status=status.HTTP_200_OK)
-                
-        else:
-            return Response({'message':"No puede eliminar una experiencia que no le pertenece"}, status=status.HTTP_403_FORBIDDEN)
         
     return Response({'message':"No se ha encontrado una experiencia con estos datos"}, status=status.HTTP_400_BAD_REQUEST)
 
--- backend/apis/api_experiences/serializers.py
+++ backend/apis/api_experiences/serializers.py
@@ -7,5 +7,12 @@ class ExperienceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Experience
         fields = '__all__'
+        extra_kwargs = {'student': {'write_only': True}}
+
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['student'] = instance.student.id
+        return representation
         
 
