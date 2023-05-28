d2eae7c816c06f1a49c0230753a5b73125f18477
jbl0107 <ballestero20017@gmail.com>
2023-05-28 14:46:07 +0200

    #89 MySQL integrado

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'estupiso',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
 
@@ -152,3 +156,6 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': True,
 
 }
+
+
+
