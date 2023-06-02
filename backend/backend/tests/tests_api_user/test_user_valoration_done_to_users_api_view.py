4d85d28c366b3ac2860ea6ef629204149fa09791
jbl0107 <ballestero20017@gmail.com>
2023-06-02 20:31:18 +0200

    #96 Tests de User realizados Se prueba el método login también

---
+++ backend/backend/tests/tests_api_user/test_user_valoration_done_to_users_api_view.py
@@ -0,0 +1,97 @@
+from faker import Faker
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+from apis.models import User
+
+class TestUserDetailApiView(APITestCase):
+
+    
+    def setUp(self):
+
+        fake = Faker('es_ES')
+        self.user = User.objects.create_user(
+            dni='22334455E',
+            name=fake.name(),
+            surname=fake.last_name(),
+            username='user1',
+            password='hola1234',
+            email=fake.email(), 
+            telephone=fake.phone_number(),
+            photo=None
+        
+        )
+
+        self.user_2 = User.objects.create_user(
+            dni='11223399P',
+            name=fake.name(),
+            surname=fake.last_name(),
+            username='user2',
+            password='hola1234',
+            email=fake.email(), 
+            telephone=fake.phone_number(),
+            photo=None
+        
+        )
+
+
+        self.admin = User.objects.create_superuser(
+            dni='22334455A',
+            name=fake.name(),
+            surname=fake.last_name(),
+            username='developer',
+            password='developer',
+            email=fake.email(), 
+            telephone=fake.phone_number(),
+            photo=None
+        )
+
+
+        login_user = {
+            'username': self.user.username,
+            'password': 'hola1234'
+        }
+        response_user = self.client.post('/login/',login_user, format='json')
+        self.assertEqual(response_user.status_code, status.HTTP_200_OK)
+
+
+        login_admin = {
+            'username': self.admin.username,
+            'password': 'developer'
+        }
+        response_admin = self.client.post('/login/', login_admin, format='json')
+        self.assertEqual(response_admin.status_code, status.HTTP_200_OK)
+        
+        self.token_user = response_user.data['access']
+        self.token_admin = response_admin.data['access']
+
+
+
+    def test_negative(self):
+        response = self.client.get(f'/users/{self.user.id}/valorations/done/users')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token_user)
+        response = self.client.get(f'/users/{self.admin.id}/valorations/done/users')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token_user)
+        response = self.client.get(f'/users/{self.user_2.id}/valorations/done/users')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token_user)
+        response = self.client.get(f'/users/{self.user_2.id+10}/valorations/done/users')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+
+    def test_positive(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token_user)
+        response = self.client.get(f'/users/{self.user.id}/valorations/done/users')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token_admin)
+        response = self.client.get(f'/users/{self.user.id}/valorations/done/users')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        
