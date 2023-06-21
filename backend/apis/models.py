a9eada8c8e31b25be617effa97327aba2dd99f42
jbl0107 <ballestero20017@gmail.com>
2023-06-22 00:55:21 +0200

    Eliminación del panel de admin Se elimina debido a conflictos con las contraseñas

--- backend/apis/models.py
+++ backend/apis/models.py
@@ -73,19 +73,6 @@ class User(AbstractBaseUser):
     def has_module_perms(self, app_label):
         return True
     
-    def save(self, *args, **kwargs):
-        self.set_password(self.password)
-        super().save(*args, **kwargs)
-
-    def clean(self):
-        super().clean()
-        if len(self.password) < 8:
-            raise ValidationError('La contraseña debe tener al menos 8 caracteres')
-            
-        nif_regex = re.compile(r'^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKE]$', re.IGNORECASE)
-        nie_regex = re.compile(r'^[XYZ][0-9]{7}[TRWAGMYFPDXBNJZSQVHLCKE]$', re.IGNORECASE)
-        if not nif_regex.match(self.dni) and not nie_regex.match(self.dni):
-            raise ValidationError('El formato del DNI no es válido')
     
     @property
     def is_staff(self):
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     path('', include('apis.urls')),
 ]
 
